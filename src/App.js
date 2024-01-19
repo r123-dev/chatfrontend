baf923ca5da809aeae08a74da94e422939c57fa4
Vivek Mourya <rohanmourya879@gmail.com>
2024-01-20 00:44:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+ import React from 'react';
+ import { BrowserRouter as Router,Route,Switch} from "react-router-dom"
+ import Authentication from './pages/authentication/authentication'
+ import Home from './pages/home/home'
+ import FourOFour from './pages/four-o-four/four-o-four';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <Router>
+     <Switch>
+        <Route path="/" exact Component={Authentication}/>
+         <Route path="/home/" Component={Home}/>
+         <Route  Component={FourOFour}/>
+     </Switch>
+
+   </Router>
+
   );
 }
 
